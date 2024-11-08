11ac1a3bf49212863d74dea1a96945f961f3def7
Franco Gonzalez <fegll87@gmail.com>
2024-11-08 12:07:25 -0300

    imgP

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Support from "./components/Support";
+import Images from "./components/Images";
 
 const App: React.FC = () => {
   return (
     <div>
       <nav>
         <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
-        <Link to="/support">Support</Link>
+        <Link to="/support">Support</Link> | <Link to="/images">Images</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/support" element={<Support />} />
+        <Route path="/images" element={<Images />} />
       </Routes>
     </div>
   );
---
+++ src/components/Images.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Images: React.FC = () => {
+  return (
+    <div>
+      <h1>Images Page</h1>
+      <p>This is the images page.</p>
+      <img
+        style={{ width: "40vw" }}
+        src="https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68"
+        alt="sl"
+      />
+    </div>
+  );
+};
+
+export default Images;
