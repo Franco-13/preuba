d2d55b544038aa008d2c38ebe6f1ec09fb9cad5c
Franco Gonzalez <fegll87@gmail.com>
2024-11-08 11:50:50 -0300

    sprt

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Support from "./components/Support";
 
 const App: React.FC = () => {
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/support">Support</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/support" element={<Support />} />
       </Routes>
     </div>
   );
---
+++ src/components/Support.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Support: React.FC = () => {
+  return (
+    <div>
+      <h1>Support Page</h1>
+      <p>This is the Support page.</p>
+      <img
+        style={{ width: "40vw" }}
+        src="https://picsum.photos/id/200/300?grayscale&blur=2"
+        alt="sl"
+      />
+    </div>
+  );
+};
+
+export default Support;
